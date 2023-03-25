25122bd7e2a4f0559c65f47c401be6d2eb3e0a7d
omar6685 <omr6685@hotmail.com>
2023-03-25 14:08:46 +0300

    remove delivery & payment & confirm - i forgot why i added the decorator

---
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -0,0 +1,19 @@
+# app/controllers/spree/checkout_controller_decorator.rb
+Spree::CheckoutController.class_eval do
+    def update
+      if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+        if @order.next
+          # Set the payment method to the default payment method (e.g. Check)
+          payment_method = Spree::PaymentMethod::Check.first
+          @order.payments.create!(payment_method: payment_method)
+  
+          render json: { success: true }
+        else
+          render json: { success: false, errors: @order.errors.full_messages }
+        end
+      else
+        render json: { success: false, errors: @order.errors.full_messages }
+      end
+    end
+  end
+  
