cc0d1fbf95fa79689fe9f35ea5bfc3bc1db30442
omar6685 <omr6685@hotmail.com>
2024-03-21 16:10:39 +0300

    test push

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserMailer < Spree::BaseMailer
-  def reset_password_instructions(user, token, *_args)
+  def reset_password_instructions(user, token, *_args) 
     @store = Spree::Store.default
     @edit_password_reset_url = edit_spree_user_password_url(reset_password_token: token, host: @store.url)
     mail to: user.email, from: from_address(@store), subject: "#{@store.name} #{I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])}"
